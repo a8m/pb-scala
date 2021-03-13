a2c8319b8a4cd0935aaa88c6b982fe245bfd7186
Bilal <bilal.m.fazlani@gmail.com>
2021-03-14 01:37:14 +0530

    update scala, sbt and dependency versions (#6)

--- build.sbt
+++ build.sbt
@@ -1,10 +1,10 @@
 name := "pb"
 version := "0.2"
-scalaVersion := "2.11.4"
+scalaVersion := "2.13.5"
 
 libraryDependencies ++= Seq(
-  "com.github.nscala-time" %% "nscala-time" % "2.2.0",
-  "org.scalatest" %% "scalatest" % "2.2.5" % "test",
+  "com.github.nscala-time" %% "nscala-time" % "2.26.0",
+  "org.scalatest" %% "scalatest" % "3.2.6" % "test",
   "jline" % "jline" % "2.13"
 )
 
--- src/test/scala/pb.scala
+++ src/test/scala/pb.scala
@@ -1,5 +1,7 @@
 package pb
-import org.scalatest._
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
 
 trait MockOutput extends Output {
   var messages: Seq[String] = Seq()
@@ -7,33 +9,32 @@ trait MockOutput extends Output {
   override def print(s: String) = messages = messages :+ s
 }
 
-class ProgressBarTest extends WordSpec with Matchers {
+class ProgressBarTest extends AnyWordSpecLike with Matchers {
 
   "A ProgressBar" when {
     "Call add()" should {
       "increment `current` in n" in {
         var pb = new ProgressBar(100) with MockOutput
         pb.add(10)
-        pb.current should be (10)
-        pb.messages.last should (
-          startWith ("\r10 / 100") and
+        pb.current should be(10)
+        pb.messages.last should (startWith("\r10 / 100") and
           endWith regex "10.00 % (\\d+)/s (\\d+s *)*".r)
       }
     }
     "Using += operator" should {
       "increment `current` in n" in {
         var pb = new ProgressBar(100) with MockOutput
-        pb+= 10
-        pb.current should be (10)
+        pb += 10
+        pb.current should be(10)
       }
     }
     "Call finish" should {
       "set `current` to `total`" in {
         var pb = new ProgressBar(1) with MockOutput
         pb.finish()
-        pb.current should be (pb.total)
-        pb.isFinish should be (true)
-        pb.messages.size should be (1)
+        pb.current should be(pb.total)
+        pb.isFinish should be(true)
+        pb.messages.size should be(1)
       }
     }
     "Set format" should {
